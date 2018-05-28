0e8b0163aa2d7efb6da54815663cb69ae39142d3
mjrv <hervejurin972@gmail.com>
2018-05-28 11:46:50 -0400

    livre

---
+++ index.php
@@ -0,0 +1,328 @@
+<!doctype html>
+	<html lang="fr">
+		<head>
+		    <meta charset="utf-8">
+		    <!-- Affichage sans zoom pour les mobiles -->
+		    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		    <!-- Pour Internet Explorer : S'assurer qu'il utilise la dernière version du moteur de rendu -->    
+    		<meta http-equiv="X-UA-Compatible" content="IE-edge">
+		    <meta name="description" content="location de logement en martinique">
+		    <meta name="author" content="webinfodom">
+		    <!-- icon a gauche du titre-->
+		    <link rel="icon" href="assets/img/icon.jpg">
+
+		    <title>oceanheights | location de logement en Martinique | excursion en voilier départ de Martinique</title>
+
+		    <!-- Bootstrap 4 CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		    <!-- Custom styles for this template -->
+		    <link href="assets/css/styles.css" rel="stylesheet">
+
+		    <!--font awesome pour les icons  -->
+		    <!-- <script src="https://use.fontawesome.com/924a5807fe.js"></script> -->
+		    <!-- <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script> -->
+		    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+
+		     <!-- HTML5 Shiv -->
+    		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js" integrity="sha256-sqQlcOZwgKkBRRn5WvShSsuopOdq9c3U+StqgPiFhHQ=" crossorigin="anonymous"></script>
+		</head>
+  	<body>
+
+	      		<nav class="navbar navbar-expand-md sticky-top bg-dark">
+	      			<div class="container-fluid">
+		        		<a class="navbar-brand" href="index.php">OCEANHEIGHTS</a>
+				        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+				          	<span class="navbar-toggler-icon"><i class="fa fa-bars text-primary" aria-hidden="true"></i></span>
+				        </button>
+				        <div class="collapse navbar-collapse" id="navbarCollapse">
+					        <ul class="navbar-nav ml-auto">
+					            <li class="nav-item active">
+					              <a class="nav-link" href="index.php">ACCUEIL<span class="sr-only">(current)</span></a>
+					            </li>
+					            <li class="nav-item">
+					              <a class="nav-link" href="logementf1.php">LOGEMENT F1</a>
+					            </li>
+					            <li class="nav-item">
+					              <a class="nav-link" href="logementf2.php">LOGEMENT F2</a>
+					            </li>
+					            <li class="nav-item">
+					              <a class="nav-link" href="voilier.php">PRESTATION EN VOILIER</a>
+					            </li>
+					            <li class="nav-item">
+					              <a class="nav-link" href="contact.php">NOUS CONTACTER</a>
+					            </li>
+					        </ul>
+				        </div>
+			        </div>
+			    </nav>
+		 
+
+    <main class="container-fluid r-p r-m">
+
+      	<div id="myCarousel" class="carousel slide r-m r-p" data-ride="carousel">
+	        <ol class="carousel-indicators">
+	          	<li data-target="#myCarousel" data-slide-to="0" class="active">1</li>
+	          	<li data-target="#myCarousel" data-slide-to="1">2</li>
+	          	<li data-target="#myCarousel" data-slide-to="2">3</li>
+	        </ol>
+        	<div class="carousel-inner col-xs-12 r-m r-p">
+        					<!-- slide1 -->
+          		<div class="carousel-item active">
+            		<img class="first-slide" src="assets/img/slide1.jpg" alt="Photo logement F1">
+            		<div class="container">
+            			<div class="carousel-caption text-left">
+                			<h1 class="f1h1">Location de 4 logements de type F1 tout équipés en Martinique.</h1>
+                			<p>" De la terrasse de votre appartement, respirez l'air du grand large, face à un magnifique panorama en avant des récifs coraliens et de l'ocean, sur 150° ".
+                			</p>
+                			<p>
+                				<a class="btn btn-lg btn-primary" href="logementf1.php" role="button">Plus d informations</a>
+                			</p>
+                     					<!-- collapse bouton :contacter nous-->
+	            			<p> 
+	  							<a class="btn btn-lg btn-primary" data-toggle="collapse" href="#contactf1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Nous contacter</a>
+	  						</p>
+  
+							<div class="row">
+							  	<div class="col">
+							    	<div class="collapse multi-collapse" id="contactf1">
+							      		<div class="card card-body">
+							       			<p style="color: black">
+								       		LES HAUTS DE L OCEAN <br>Quartier PEROU <br> 97290 LE MARIN <br>
+								         	Email:<a href="mailto:oceanheights@orange.fr"> oceanheights@orange.fr</a> <br>
+								       		Téléphone : 0596 74 78 61 <br>
+								       		Cellulaires : 0696 26 24 13 // 0696 93 09 58
+								       		</p>
+							      		</div>
+							    	</div>
+							  	</div>
+				            </div>
+  							<!-- end collapse bouton :contacter nous-->
+              			</div>
+            		</div>
+          		</div>
+          		<!-- slide2 -->
+		        <div class="carousel-item">
+		            <img class="second-slide" src="assets/img/slide2.jpg" alt="Photo logement F2">
+		            <div class="container">
+		              	<div class="carousel-caption">
+		                	<h1>Location d'un logement de type F2 tout équipé en Martinique.</h1>
+		                	<p>" De la terrasse de votre appartement, respirez l'air du grand large, face à un magnifique panorama en avant des récifs coraliens et de l'ocean, sur 150° ".
+		                	</p>
+		                	<p>
+		                		<a class="btn btn-lg btn-primary" href="logementf2.php" role="button">Plus d informations</a>
+		                	</p>
+		                     					<!-- collapse bouton :contacter nous-->
+				            <p> 
+				  				<a class="btn btn-lg btn-primary" data-toggle="collapse" href="#contactf2" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Nous contacter</a>
+				  			</p>
+		  
+							<div class="row">
+							  	<div class="col">
+							    	<div class="collapse multi-collapse" id="contactf2">
+							      		<div class="card card-body">
+										    <p style="color: black">
+										       	LES HAUTS DE L OCEAN <br>Quartier PEROU <br> 97290 LE MARIN <br>
+										        Email:<a href="mailto:oceanheights@orange.fr"> oceanheights@orange.fr</a><br>
+										       	Téléphone : 0596 74 78 61 <br>
+										       	Cellulaires : 0696 26 24 13 // 0696 93 09 58
+										    </p>
+							      		</div>
+							    	</div>
+							  	</div>
+				            </div>
+		  							<!-- end collapse bouton :contacter nous-->
+		              	</div>
+		            </div>
+		        </div>
+          		<!-- slide3 -->
+        		<div class="carousel-item">
+            		<img class="third-slide" src="assets/img/slide3.jpg" alt="photo voilié">
+            		<div class="container">
+            			<div class="carousel-caption text-right" id="slide3">
+                			<h1>Excursion en voilier</h1>
+			                <p><span> Excursion en voilier - direction Sainte lucie et sa "friday nigth party" - départ le vendredi matin et retour le dimanche midi</span> <br> Tarifs: <br>  60 € TTC le transport par personne <br>40€ TTC le logement par nuit pour 1-2 personnes.
+			                </p
+			                <p>
+			                	<a class="btn btn-lg btn-primary" href="voilier.php" role="button">Plus d informations</a>
+			                </p>
+			                					<!-- collapse bouton :contacter nous-->
+				            <p> 
+				  				<a class="btn btn-lg btn-primary" data-toggle="collapse" href="#contactvoilier" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Nous contacter</a>
+				  			</p>
+  
+							<div class="row">
+							  	<div class="col">
+							    	<div class="collapse multi-collapse" id="contactvoilier">
+							      		<div class="card card-body">
+									       <p style="color: black">
+									       		LES HAUTS DE L OCEAN <br>Quartier PEROU <br> 97290 LE MARIN <br>
+									         	Email:<a href="mailto:oceanheights@orange.fr"> oceanheights@orange.fr</a><br>
+									       		Téléphone : 0596 74 78 61 <br>
+									       		Cellulaires : 0696 26 24 13 // 0696 93 09 58
+									       </p>
+							      		</div>
+							    	</div>
+							  	</div>
+				            </div>
+  							<!-- end collapse bouton :contacter nous-->
+            			</div>
+          			</div>
+        		</div>
+        		<!-- end caroussel inner -->
+    		</div>
+		        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+		          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		          <span class="sr-only">Previous</span>
+		        </a>
+		        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+		          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		          <span class="sr-only">Next</span>
+		        </a>
+      	</div>  
+
+      				<!-- end caroussel slider -->
+
+
+      				<!-- after slider 3 pictures rondes avec effet grossissant et le texte en bas
+      						================================================== -->
+      				<!-- 3 boutons details et un bouton contacter nous . -->
+
+      	<div class="container marketing">
+
+        			<!-- Three columns with picture and text -->
+        	<div class="row">
+		        <div class="col-lg-4">
+		            <img class="rounded-circle" src="assets/img/f1-sejour-lit-double.jpg" alt="Photo logement F1" width="200" height="200">
+		            <h2>Logement F1</h2>
+		            <p>
+		            	Location de logement de type F1 <br>à partir de 126 € pour 3 nuits <br> de la terrasse de votre appartement, respirez l'air du grand large, face à un magnifique panorama en avant des récifs coralliens et de l'Océan, sur 150°.
+		            </p>
+		            <p>
+		            	<a class="btn btn-secondary" href="logementf1.php" role="button">Voir détail &raquo;</a>
+		            </p>
+		        </div><!-- end logement f1 -->
+		        <div class="col-lg-4">
+		            <img class="rounded-circle" src="assets/img/f2-sejour.jpg" alt="Photo logement F2" width="200" height="200">
+		            <h2>Logement F2</h2>
+		            <p>
+		            	Location de logement de type F2 <br>à partir de 174 € pour 3 nuits <br>de la terrasse de votre appartement, respirez l'air du grand large, face à un magnifique panorama en avant des récifs coralliens et de l'Océan, sur 150°.
+		            </p>
+		            <p>
+		            	<a class="btn btn-secondary" href="logementf2.php" role="button">Voir détail  &raquo;</a>
+		            </p>
+		        </div><!-- end logement f2 -->
+		        <div class="col-lg-4">
+		            <img class="rounded-circle" src="assets/img/oceanissmall.jpg" alt="Photo VOILIER" width="200" height="200">
+		            <h2>Excursion en voilier</h2>
+		            <p>
+		            	Une excursion en voilier à sainte lucie pour 60€ TTC. <br>vous aurez 2 jours pour decouvrir sainte lucie et surtout la friday nigth pour ceux qui aiment les endroits ambiancés (restaurants, bars, musiques en pleine rue).
+		            </p>
+		            <p>
+		            	<a class="btn btn-secondary" href="voilier.php" role="button">Voir détail  &raquo;</a>
+		            </p>
+		        </div><!-- end excursion en voilier -->
+          		<div class="col-lg text-center">
+          						<!-- collapse bouton :contacter nous-->
+		            <p> 
+		  				<a class="btn btn-primary" data-toggle="collapse" href="#contactlogement" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Nous contacter</a>
+		  			</p>
+  
+					<div class="row">
+					  	<div class="col">
+					    	<div class="collapse multi-collapse" id="contactlogement">
+					      		<div class="card card-body">
+								    LES HAUTS DE L OCEAN <br>Quartier PEROU <br> 97290 LE MARIN <br>
+								    <p>
+								    Email:<a href="mailto:oceanheights@orange.fr"> oceanheights@orange.fr</a> <br>
+								    Téléphone : 0596 74 78 61 <br>
+								    Cellulaires : 0696 26 24 13 // 0696 93 09 58
+					       			</p>
+					      		</div>
+					    	</div>
+					  	</div>
+					</div>
+  							<!-- end collapse bouton :contacter nous-->
+          		</div>
+        	</div><!-- /.row -->
+
+
+        <!-- THE FEATURETTES logement et excursion 2 pictures with effect grossissant and description and 1 button contacter nous-->
+
+        	<hr class="featurette-divider">
+
+        	<div class="row featurette">
+          		<div class="col-md-7">
+            		<h2 class="featurette-heading">Location appartement et excursion en voilier <span class="text-muted">sur une semaine.</span></h2>
+            		<p class="lead">
+            			Location d un appartement avec une excursion en voilier à sainte lucie à partir de 480 € pour 2 personnes. <br>vous aurez 2 jours pour decouvrir sainte lucie et surtout la friday nigth pour ceux qui aiment les endroits ambiancés (restaurants, bars, musiques en pleine rue).
+            		</p>
+							<!-- collapse bouton :contacter nous-->
+		            <p> 
+		  				<a class="btn btn-primary" data-toggle="collapse" href="#contactsemaine" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Nous contacter</a>
+		  			</p>
+  
+					<div class="row">
+					  	<div class="col">
+					    	<div class="collapse multi-collapse" id="contactsemaine">
+					      		<div class="card card-body">
+					       		LES HAUTS DE L OCEAN <br>Quartier PEROU <br> 97290 LE MARIN <br>
+						        <p>
+						        Email:<a href="mailto:oceanheights@orange.fr"> oceanheights@orange.fr</a> <br>
+						       	Téléphone : 0596 74 78 61 <br>
+						       	Cellulaires : 0696 26 24 13 // 0696 93 09 58
+						       </p>
+					      	</div>
+					    </div>
+					  </div>
+					</div>
+  							<!-- end collapse bouton :contacter nous-->
+          		</div>
+          		<!-- 2 pictures with effect -->
+		        <div class="col-md-5 text-center">
+		            <img class="featurette-image img-fluid d-block img-thumbnail" src="assets/img/oceanis1.jpg" alt="VOILIER" width="75%">
+		            <img class="featurette-image img-fluid d-block img-thumbnail " src="assets/img/stlucie.jpg" alt="sainte lucie" width="75%">
+		        </div>
+        	</div>
+        
+        					<!-- /END THE FEATURETTES -->
+
+      	</div><!-- /.container -->
+
+ 
+      						<!-- FOOTER -->
+	    <footer class="container">
+		    <div class="row">
+				<div class="col-md-6 text-center contact"> <br> <br>
+				    <p>LES HAUTS DE L OCEAN <br>Quartier PEROU <br> 97290 LE MARIN</p>
+				    <p>
+				    Email:<a href="mailto:oceanheights@orange.fr"> oceanheights@orange.fr</a> <br>
+				    Téléphone : 0596 74 78 61 <br>
+				    Cellulaires : 0696 26 24 13 // 0696 93 09 58
+					</p>
+				</div>
+		      	<div class="col-md-6 text-center">
+				        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3862.6769314565604!2d-60.8676212852972!3d14.503226483344058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c4020d24b61e151%3A0xa2f97466c32083a4!2sPerou%2C+Le+Marin%2C+Martinique!5e0!3m2!1sfr!2s!4v1523992692364" width="330" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+				<div class="col-md-12">
+				 	<p class="float-right">
+				 		<a href="index.php">haut de la page</a>
+				 	</p>
+				</div>
+				<div class="col-md-12">
+				    <p>
+				        &copy; 2018 webinfodom, Inc. &middot; <a href="http://www.webinfodom.com/">site web</a> &middot;
+				    </p>
+				</div>
+		     </div>
+	    </footer>
+    </main>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ logementetvoilier.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="fr">
+ <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/img/icon.jpg">
+    <!--font awesome pour les icons  -->
+    <script src="https://use.fontawesome.com/924a5807fe.js"></script>
+    <!-- <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script> -->
+
+    <title>oceanheights | logement en martinique | excursion voilier</title>
+
+    <!-- Bootstrap core CSS -->
+    <!-- <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- Bootstrap 4 CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/styles.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <header>
+      <nav class="navbar navbar-expand-md sticky-top bg-dark">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">OCEANHEIGHTS</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"><i class="fa fa-bars text-primary" aria-hidden="true"></i></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                  <ul class="navbar-nav ml-auto">
+                      <li class="nav-item active">
+                        <a class="nav-link" href="index.php">ACCUEIL<span class="sr-only">(current)</span></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="logementf1.php">LOGEMENT F1</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="logementf2.php">LOGEMENT F2</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="voilier.php">PRESTATION EN VOILIER</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact.php">NOUS CONTACTER</a>
+                      </li>
+                  </ul>
+                </div>
+              </div>
+          </nav>
+    </header>
+
+    <main role="main">
+
+            <!-- FOOTER -->
+      <footer class="container">
+	      	<div class="row">
+            <div class="col-md-6 text-center contact"> <br> <br>
+              <p>LES HAUTS DE L OCEAN <br>Quartier PEROU <br> 97290 LE MARIN</p>
+              <p>
+              Email:<a href="mailto:oceanheights@orange.fr"> oceanheights@orange.fr</a> <br>
+              Téléphone : 0596 74 78 61 <br>
+              Cellulaires : 0696 26 24 13 // 0696 93 09 58
+              </p>
+            </div>
+            <div class="col-md-6 text-center">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3862.6769314565604!2d-60.8676212852972!3d14.503226483344058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c4020d24b61e151%3A0xa2f97466c32083a4!2sPerou%2C+Le+Marin%2C+Martinique!5e0!3m2!1sfr!2s!4v1523992692364" width="330" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+            <div class="col-md-12">
+              <p class="float-right">
+                <a href="logementetvoilier.php">haut de la page</a>
+              </p>
+            </div>
+            <div class="col-md-12">
+              <p>
+                    &copy; 2018 webinfodom, Inc. &middot; <a href="http://www.webinfodom.com/">site web</a> &middot;
+              </p>
+            </div>
+          </div>
+      </footer>
+    </main>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
